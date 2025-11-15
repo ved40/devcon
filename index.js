c9039a5794a389f0cbd7a8d5a5ab4c7e84b4869d
ved40 <ved.cr4045@gmail.com>
2025-11-15 15:00:42 +0530

    basic routes

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/devcon");
+}
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/todo", (req, res) => {
+    res.render("todo.ejs")
+})
 
 app.listen(8080, () => {
     console.log("app is listening on the port 8080")
